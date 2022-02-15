afba7959f5f587af6c071853de3bc6576cf8a76f
bear <caojiaxin@bilibili.com>
2022-02-15 17:24:21 +0800

    add okio

--- app/src/main/java/com/bear/arcdemo/showcode/PictureDownload.kt
+++ app/src/main/java/com/bear/arcdemo/showcode/PictureDownload.kt
@@ -6,8 +6,10 @@ import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.bear.arcdemo.bearLog
+import okio.*
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
 import java.net.URL
 import java.util.*
 
@@ -59,7 +61,18 @@ class PictureDownload private constructor() {
             doInput = true
         }
         val inputStream = http.getInputStream()
+//        saveFile(context, name, inputStream, onProcess)
+        saveFileByOkio(context, name, inputStream, http.contentLength, onProcess)
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun saveFile(
+        context: Context,
+        name: String,
+        inputStream: InputStream,
+        onProcess: (Int) -> Unit
+    ) {
         val path = context.dataDir.absolutePath + File.separator + "pic"
         val file = File(path)
         if (!file.exists()) {
@@ -81,7 +94,49 @@ class PictureDownload private constructor() {
         } while (true)
         inputStream.close()
         fos.close()
+    }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun saveFileByOkio(
+        context: Context,
+        name: String,
+        inputStream: InputStream,
+        length: Int,
+        onProcess: (Int) -> Unit
+    ) {
+        var bufferedSink: BufferedSink? = null
+        var bufferSource: Source? = null
+        try {
+            val source = inputStream.source()
+            val path = context.dataDir.absolutePath + File.separator + "pic"
+            val picDic = File(path)
+            if (!picDic.exists()) {
+                picDic.mkdir()
+            }
+
+            val file = File(path + File.separator + "$name.png")
+            bufferedSink = file.sink().buffer()
+            bufferSource = source(length, source, onProcess)
+            bufferedSink.writeAll(bufferSource)
+            bufferedSink.flush()
+        } catch (e: Exception) {
+            bearLog(e.toString())
+        } finally {
+            bufferedSink?.close()
+            bufferSource?.close()
+        }
+    }
+
+    private fun source(contentLength: Int, source: Source, onProcess: (Int) -> Unit): Source {
+        return object : ForwardingSource(source) {
+            var size: Long = 0L
+            override fun read(sink: Buffer, byteCount: Long): Long {
+                val byte = super.read(sink, byteCount)
+                size += if (byte == -1L) 0 else byte
+                onProcess((size * 100 / contentLength).toInt())
+                return byte
+            }
+        }
     }
 
     companion object {
