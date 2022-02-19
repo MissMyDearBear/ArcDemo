c13760f15ef2b55d42dce39f90d62254f63a18f8
bear <caojiaxin@bilibili.com>
2022-02-19 17:02:12 +0800

    opt cpp

--- app/src/main/cpp/native-log.cpp
+++ app/src/main/cpp/native-log.cpp
@@ -13,22 +13,8 @@ using namespace std;
 
 extern "C"
 void Java_com_bear_arcdemo_ndk_NLog_nBearLog(JNIEnv *env, jclass s, jstring jStr) {
-//    const jclass stringClass = env->GetObjectClass(jStr);
-//    const jmethodID getBytes = env->GetMethodID(stringClass, "getBytes", "(Ljava/lang/String;)[B");
-//    const jbyteArray stringJBytes = (jbyteArray) env->CallObjectMethod(jStr, getBytes,
-//                                                                       env->NewStringUTF("UTF-8"));
-//    size_t length = (size_t) env->GetArrayLength(stringJBytes);
-//    jbyte *pBytes = env->GetByteArrayElements(stringJBytes, NULL);
-//
-//    string ret = string((char *) pBytes, length);
-//    env->ReleaseByteArrayElements(stringJBytes, pBytes, JNI_ABORT);
-//    int n = ret.length();
-//    char char_array[n + 1];
-//    strcpy(char_array, ret.c_str());
-//    LOGI("%s", char_array);
-
-    const char *nativeString = env->GetStringUTFChars(jStr, NULL);
-    env->ReleaseStringUTFChars(jStr, nativeString);
+    const char *nativeString = env->GetStringUTFChars(jStr, nullptr);
     LOGI("%s", nativeString);
-    nativeString = NULL;
+    //使用完后要主动释放
+    env->ReleaseStringUTFChars(jStr, nativeString);
 }
